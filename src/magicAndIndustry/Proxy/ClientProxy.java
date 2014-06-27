59dfb40696f900181fe14e6e216ac46332f0b4a4
Snirk Immington <snirk.immington@gmail.com>
2014-06-27 08:41:47 -0400

    Removed more files

--- src/magicAndIndustry/Proxy/ClientProxy.java
+++
@@ -1,7 +0,0 @@
-package magicAndIndustry.Proxy;
-
-
-public class ClientProxy extends ServerProxy
-{
-	public void registerRenderThings() { }
-}
--- src/magicAndIndustry/Proxy/ServerProxy.java
+++
@@ -1,6 +0,0 @@
-package magicAndIndustry.Proxy;
-
-public class ServerProxy 
-{
-	public void registerRenderThings() { }
-}
