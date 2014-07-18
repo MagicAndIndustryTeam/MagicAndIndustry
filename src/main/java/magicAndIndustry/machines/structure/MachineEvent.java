bf8197919ab3771d5b14d44efae351f6eedd5777
Snirk Immington <snirk.immington@gmail.com>
2014-07-18 10:31:04 -0400

    Manually rm'd unused files, finally

--- src/main/java/magicAndIndustry/machines/structure/MachineEvent.java
+++
@@ -1,9 +0,0 @@
-package magicAndIndustry.machines.structure;
-
-public abstract class MachineEvent 
-{
-	/**
-	 * Set to true if you don't want the event to continue.
-	 */
-	private boolean handled;
-}
